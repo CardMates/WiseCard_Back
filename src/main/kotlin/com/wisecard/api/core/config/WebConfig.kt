86dc717dd24bb252330904115433f0b9f9a43b34
sominyun <somin455@naver.com>
2025-11-14 04:00:53 +0900

    feat: cors 추가

--- src/main/kotlin/com/wisecard/api/core/config/WebConfig.kt
+++ src/main/kotlin/com/wisecard/api/core/config/WebConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig() : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("", "http://localhost:3000")
+            .allowedOrigins("https://wisecard.kro.kr", "http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true)
